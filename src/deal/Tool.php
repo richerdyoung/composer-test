a36970ce168076deea8be13fce91a0492c0856e1
依然范儿特西 <3151775165@qq.com>
2020-03-03 18:41:34 +0800

    first

---
+++ src/deal/Tool.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace src\deal;
+
+class Tool{
+
+    public function __construct(){
+
+    }
+
+    public static function hello(){
+        echo  'hello laofan';
+    }
+
+
+}
+
+
+?>
